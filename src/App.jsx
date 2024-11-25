ab9bded06d89851fffb4edb114367ce3a3a8a627
eberegit <samsoneberenjoku@gmail.com>
2024-11-25 08:55:44 +0100

    Debounce Tutorial Boilerplate

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import top100Films from "./assets/data";
-import Btn from "./components/btn";
 import Heading from "./components/heading";
 import Input from "./components/input";
 import Result from "./components/result";
 
 function App() {
   const [label, setLabel] = useState("");
-  const [year, setYear] = useState(null);
   const [results, setResults] = useState([]);
 
   // regular search
   const searchItems = (searchTerm) => {
+    if (!searchTerm) return;
+
     const regex = /\s+/g;
 
     const normalizeTerm = searchTerm.toLowerCase().replace(regex, "");
@@ -23,35 +23,9 @@ function App() {
     setResults(output);
   };
 
-  // search using OR (||) operator
-  const searchItemsOR = (label, year) => {
-    const regex = /\s+/g;
-
-    const normalizeTerm = label.toLowerCase().replace(regex, "");
-
-    const output = top100Films.filter(
-      (film) =>
-        film.label.toLowerCase().replace(regex, "").includes(normalizeTerm) ||
-        film.year.toString().includes(year)
-    );
-
-    setResults(output);
-  };
-
-  // search using AND (&&) operator
-  const searchItemsAND = (label, year) => {
-    const regex = /\s+/g;
-
-    const normalizeTerm = label.toLowerCase().replace(regex, "");
-
-    const output = top100Films.filter(
-      (film) =>
-        film.label.toLowerCase().replace(regex, "").includes(normalizeTerm) &&
-        film.year.toString().includes(year)
-    );
-
-    setResults(output);
-  };
+  useEffect(() => {
+    searchItems(label);
+  }, [label]);
 
   return (
     <main>
@@ -66,29 +40,10 @@ function App() {
         value={label}
         setter={setLabel}
       />
-      <Input
-        type={"number"}
-        placeholder={"Enter Year"}
-        name={"Year"}
-        value={year}
-        setter={setYear}
-      />
-
-      <br />
-
-      {/* buttons */}
-      <Btn label={"Search"} handleClick={() => searchItems(label)} />
-      <Btn
-        label={"Search (OR)"}
-        handleClick={() => searchItemsOR(label, year)}
-      />
-      <Btn
-        label={"Search (AND)"}
-        handleClick={() => searchItemsAND(label, year)}
-      />
 
       {/* results */}
       <Heading label={"Search Results"} />
+
       <section style={{ height: 400, overflow: "scroll" }}>
         {results.map(({ label, year }) => (
           <Result name={label} year={year} key={label + year} />
